bdd0fc56860685ae36016c037c5871a30dbddf7f
Valerie325 <morevimgh14@gmail.com>
2024-12-09 12:07:55 +0000

    changes

---
+++ src/index.js
@@ -0,0 +1,32 @@
+let dom
+=document.querySelector(".dom")
+dom.backgroundColor="pink"
+dom.style.color = "orange"
+dom.innerHTML ="I know now what the DOM is❤️✌️"
+
+
+let access
+=document.querySelector(".access")
+access.backgroundColor = "aqua"
+access.style.color = "white"
+access.innerHTML ="I know now what access/select elements in Dom "
+
+
+let use
+=document.querySelector(".use")
+use.style.backgroundColor = "yellow"
+use.style.color = "blue"
+use.innerHTML = "I know how to style methods to change the style of an element in Dom "
+
+
+let style
+ =document.querySelector(".style")
+ style.style.backgroundColor = "green"
+ style.stylecolor = "white"
+ style.innerHTML = "I know how to use styling properties differ in JS from CSS"
+
+ let excited
+ =document.querySelector(".excited")
+ excited.style.color = "green"
+ excited.innerHTML = "Yes I did it !! I manipulated theDOM and styled my element hurray💅🏽🌚"
+ excited.style.background = "cyan"
